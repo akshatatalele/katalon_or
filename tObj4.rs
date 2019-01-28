3e1f2cd31594363cdb18abcd6dee7323a58edd60
akshatatalele <akshata.talele@gmail.com>
2019-01-28 14:21:41 +0530

    commit1_2

---
+++ tObj4.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tObj4</name>
+   <tag></tag>
+   <elementGuidId>485e848f-00a4-4ab9-85e7-88e9f77350ec</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>name4</value>
+   </webElementProperties>
+</WebElementEntity>
