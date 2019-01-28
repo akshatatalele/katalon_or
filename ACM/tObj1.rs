7a0d329d758c978fb563a50f530abb72026d0a97
akshatatalele <akshata.talele@gmail.com>
2019-01-28 13:52:58 +0530

    fourth

---
+++ ACM/tObj1.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tObj1</name>
+   <tag></tag>
+   <elementGuidId>3688f646-117f-4407-8610-50b6844074f4</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ tObj1.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tObj1</name>
+   <tag></tag>
+   <elementGuidId>b093d5b5-aa15-4a5a-991d-999ec175a1b7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
