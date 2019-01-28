26e1490dbd56fbc478ca5178996f39c30135c626
akshatatalele <akshata.talele@gmail.com>
2019-01-28 12:30:27 +0530

    Create testObject.rs

---
+++ testObject.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>testObject</name>
+   <tag></tag>
+   <elementGuidId>3cc01fed-a219-4cc2-895a-ca82891b5d7a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'Obj1']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>Obj1</value>
+   </webElementProperties>
+</WebElementEntity>
