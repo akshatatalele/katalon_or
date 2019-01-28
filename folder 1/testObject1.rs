5f3687a693d5514dc2bf9aa1ea73572704d76f69
akshatatalele <akshata.talele@gmail.com>
2019-01-28 12:35:40 +0530

    Create testObject1.rs

---
+++ folder 1/testObject1.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>testObject1</name>
+   <tag></tag>
+   <elementGuidId>032acf99-0aff-4b73-a715-91dc1c36502b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'class']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>class</value>
+   </webElementProperties>
+</WebElementEntity>
