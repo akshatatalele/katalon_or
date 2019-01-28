9186be37ab08644c576aac11fa418e480e841f94
akshatatalele <akshata.talele@gmail.com>
2019-01-28 12:59:48 +0530

    Create testObject3.rs

---
+++ testObject3.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>testObject3</name>
+   <tag></tag>
+   <elementGuidId>c0403149-aa54-4e5f-a2ab-2f83e0e01f7c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
